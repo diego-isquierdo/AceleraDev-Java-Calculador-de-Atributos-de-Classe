b7869b8082c3a056a6468a93f5cd53fcf83c0cfd
Diego Isquierdo <diego.isquierdo@hotmail.com>
2020-05-06 00:02:27 -0300

    Condluído desafio

--- src/main/java/com/challenge/desafio/CalculadorDeClasses.java
+++ src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -1,4 +1,50 @@
 package com.challenge.desafio;
 
-public class CalculadorDeClasses {
+import com.challenge.annotation.Somar;
+import com.challenge.annotation.Subtrair;
+import com.challenge.interfaces.Calculavel;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+public class CalculadorDeClasses implements Calculavel {
+
+    @Override
+    public BigDecimal somar(Object object) {
+        return calcular(object, Somar.class);
+    }
+
+    @Override
+    public BigDecimal subtrair(Object object) {
+        return calcular(object, Subtrair.class);
+    }
+
+    @Override
+    public BigDecimal totalizar(Object object) {
+        return somar(object).subtract(subtrair(object));
+    }
+
+    private BigDecimal calcular(Object object, Class<? extends Annotation> annototation) {
+
+        return Arrays.asList(getFields(object))
+                .stream()
+                .filter(field -> field.isAnnotationPresent(annototation))
+                .map(field -> filter(field, object))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private Field[] getFields(Object object){
+        return object.getClass().getDeclaredFields();
+    }
+
+    private BigDecimal filter(Field field, Object object){
+        try{
+            field.setAccessible(true);
+            return (BigDecimal) field.get(object);
+        }catch (IllegalAccessException e){
+            return BigDecimal.ZERO;
+        }
+    }
 }
--- src/main/java/com/challenge/interfaces/Calculavel.java
+++ src/main/java/com/challenge/interfaces/Calculavel.java
@@ -3,7 +3,7 @@ package com.challenge.interfaces;
 import java.math.BigDecimal;
 
 public interface Calculavel {
-    public BigDecimal somar();
-    public BigDecimal subtrair();
-    public void totalizar();
+    public BigDecimal somar(Object object);
+    public BigDecimal subtrair(Object object);
+    public BigDecimal totalizar(Object object);
 }
